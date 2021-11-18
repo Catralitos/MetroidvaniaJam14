600f363f2b7862541485ae37f565365e41cdafb8
Carlos Marques <catralitos11@gmail.com>
2021-11-18 18:10:02 +0000

    Implementei duplo salto

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,20 +6,27 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")] public float jumpForce;
     public float jumpTime;
     public float moveSpeed;
+    public int numberOfMidairJumps;
 
     [Header("Physics Checks")] public float checkRadius;
     public LayerMask whatIsGround;
     public Transform feetPos;
 
+    private bool _facingRight;
     private bool _isGrounded;
     private bool _isJumping;
-    
+    private bool _isSomersaulting;
+
     private float _jumpTimeCounter;
+    private int _midairJumps;
+
     private Rigidbody2D _rb;
+
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _facingRight = true;
     }
 
     private void Update()
@@ -29,12 +34,23 @@ public class PlayerMovement : MonoBehaviour
         _isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
     }
 
+    //This function is called every FixedUpdate on PlayerControls
     public void Move(float xInput, bool jump)
     {
+        if (_facingRight && xInput < 0)
+        {
+            Flip();
+        }
+        
         if (jump)
         {
             if (_isGrounded)
             {
+                if (Math.Abs(xInput) <= 0.1)
+                {
+                    _isSomersaulting = true;
+                }
+
                 _isJumping = true;
                 _jumpTimeCounter = jumpTime;
                 _rb.velocity = Vector2.up * jumpForce;
@@ -51,19 +67,34 @@ public class PlayerMovement : MonoBehaviour
                     _isJumping = false;
                 }
             }
+            else if (_midairJumps < numberOfMidairJumps)
+            {
+                _midairJumps++;
+                if (Math.Abs(xInput) <= 0.1)
+                {
+                    _isSomersaulting = true;
+                }
+
+                _isJumping = true;
+                _jumpTimeCounter = jumpTime;
+                _rb.velocity = Vector2.up * jumpForce;
+            }
         }
         else
         {
+            _midairJumps = 0;
+            _isSomersaulting = false;
             _isJumping = false;
         }
-
-        /*if (!jump && !holdJump)
-        {
-            //Debug.Log("IF 5");
-
-            _isJumping = false;
-        }*/
-
+        
         _rb.velocity = new Vector2(moveSpeed * xInput, _rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
