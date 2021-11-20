0141bde58d76c69e50812fde21fc334058292cee
Carlos Marques <catralitos11@gmail.com>
2021-11-20 17:01:43 +0000

    Underwater controls feitos

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -22,17 +22,25 @@ public class PlayerEntity : MonoBehaviour
 
     public bool isCrouched;
     public bool isMorphed;
+    public bool isUnderwater;
 
     public bool unlockedDash;
     public bool unlockedDoubleJump;
+    public bool unlockedGravitySuit;
     public bool unlockedMorphBall;
     public bool unlockedTripleBeam;
     public bool unlockedPiercingBeam;
-  
-    
+
+    public float defaultDrag;
+    public float defaultGravity;
+    public float defaultMass;
+    public float underwaterDrag;
+    public float underwaterGravity;
+    public float underwaterMass;
     
     public SpriteRenderer testCylinder;
-    
+
+    private Rigidbody2D _rb;
     void Awake()
     {
         if (Instance == null)
@@ -58,8 +66,27 @@ public class PlayerEntity : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
+        if (isUnderwater && !unlockedGravitySuit)
+        {
+            _rb.drag = underwaterDrag;
+            _rb.gravityScale = underwaterGravity;
+            _rb.mass = underwaterMass;
+        }
+        else
+        {
+            _rb.drag = defaultDrag;
+            _rb.gravityScale = defaultGravity;
+            _rb.mass = defaultMass;
+        }
+
+
         if (!isCrouched && !isMorphed)
         {
             states[1].SetActive(false);
