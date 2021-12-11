2a9529c89b68f315e6a778aaa911e5a3fbfd9984
Carlos Marques <catralitos11@gmail.com>
2021-12-11 12:49:26 +0000

    Comecei consturtor de save system

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SaveSystem : MonoBehaviour
+public class SaveSystem 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float currentHealth;
+    public float elapsedTime;
+    public int extraHealthUnlocked;
+    public int extraDamageUnlocked;
+    public bool[] unlockedPowers;
+    public float[] buffTimers;
+    public float[] playerPosition;
 
-    // Update is called once per frame
-    void Update()
+    public SaveSystem(PlayerEntity playerEntity)
     {
+        currentHealth = playerEntity.Health.currentHealth;
+        unlockedPowers = new bool[6];
+        unlockedPowers[0] = playerEntity.unlockedDash;
+        unlockedPowers[1] = playerEntity.unlockedDoubleJump;
+        unlockedPowers[2] = playerEntity.unlockedGravitySuit;
+        unlockedPowers[3] = playerEntity.unlockedMorphBall;
+        unlockedPowers[4] = playerEntity.unlockedPiercingBeam;
+        unlockedPowers[5] = playerEntity.unlockedTripleBeam;
+        playerPosition = new float[3];
+        playerPosition[0] = playerEntity.gameObject.transform.position.x;
+        playerPosition[1] = playerEntity.gameObject.transform.position.y;
+        playerPosition[2] = playerEntity.gameObject.transform.position.z;
         
+
     }
 }
