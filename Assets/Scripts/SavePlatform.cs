3af0a0941f01e9a6f7c8f169839e4e0b56000be3
Carlos Marques <catralitos11@gmail.com>
2021-12-14 15:22:02 +0000

    Bug fix na save platform

--- Assets/Scripts/SavePlatform.cs
+++ Assets/Scripts/SavePlatform.cs
@@ -1,3 +1,4 @@
+using System;
 using Extensions;
 using GameManagement;
 using Player;
@@ -5,11 +6,23 @@ using UnityEngine;
 
 public class SavePlatform : MonoBehaviour
 {
+    
     public LayerMask playerMask;
+    private bool _canSave;
+
+    private void Start()
+    {
+        Invoke(nameof(CanSave), 3f);
+    }
+
+    private void CanSave()
+    {
+        _canSave = true;
+    }
     
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (playerMask.HasLayer(other.gameObject.layer) && !LevelManager.Instance.countingDown)
+        if (_canSave && playerMask.HasLayer(other.gameObject.layer) && !LevelManager.Instance.countingDown)
         {
             SaveSystem.SavePlayer(PlayerEntity.Instance);
             PlayerEntity.Instance.UI.DisplaySaveTooltip();
